13b17815b6ed786c2562884489c34e471186f0a4
Wuang Qin <wuang@leohealth.com>
2015-07-09 18:27:28 -0400

    add the required component to the app component

--- src/scripts/components/app.jsx
+++ src/scripts/components/app.jsx
@@ -43,3 +43,13 @@
 //            </div>
 //        </footer>
 //    </div>
+
+var React = require('react/addons');
+var Router = require('react-router');
+var Reflux = require('reflux');
+
+var LoginActions = require('../actions/loginActions');
+var RouterActions = require('../actions/routerActions');
+var SessionStore = require();
+var RouteStore = require();
+var PasswordStore = require();
--- src/scripts/components/pages/login.jsx
+++ src/scripts/components/pages/login.jsx
@@ -6,7 +6,7 @@ module.exports = React.createClass({
   render: function(){
     return(
       <div>
-        <LoginForm/>
+        I am here
       </div>
     )
   }
