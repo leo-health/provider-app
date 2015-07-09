46615fa4f849d29ee9bef9241964d0d55fe24402
Wuang Qin <wuang@leohealth.com>
2015-07-09 15:19:43 -0400

    finshed implementing react routes and added the route for default and login

---
+++ src/scripts/components/router.jsx
@@ -0,0 +1,19 @@
+var React = require('react')
+//# Assign React to Window so the Chrome React Dev Tools will work.
+window.React = React
+
+var Router = require('react-router')
+
+var App = require('./app')
+var Login = require('.pages/login')
+
+var routes = (
+  <Route handler={App} path='/'>
+    <DefaultRoute handler={Login}/>
+    <Route name="login" handler={Login}/>
+  </Route>
+)
+
+Router.run(routes, function (Handler) {
+  React.render(<Handler/>, document.getElementById("container"));
+});
