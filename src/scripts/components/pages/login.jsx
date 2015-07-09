2f565ff80bdf2520362df5fcd1591439cb0880cc
Wuang Qin <wuang@leohealth.com>
2015-07-09 15:39:19 -0400

    finish rolling the login page component, a wrapper of the login form component(not the login form component)

---
+++ src/scripts/components/pages/login.jsx
@@ -0,0 +1,13 @@
+var React = require('react')
+
+var LoginForm = require('../modules/login/login-form')
+
+module.exports = React.createClass({
+  render: function(){
+    return(
+      <div>
+        <LoginForm/>
+      </div>
+    )
+  }
+});
