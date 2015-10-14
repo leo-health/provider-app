c5c938c4be93ae3f6cbddf733b7e4c126cf8b3d9
Nayan Jain <nayan@leohealth.com>
2015-10-14 13:37:57 -0400

    devops - remove path/engine

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 var express = require("express");
-var engine = require("path")
-
 var app = express();
 
 app.use(express.static(__dirname + '/public'))
-
 
 var port = process.env.PORT || 8081;
 app.listen(port, function() {
