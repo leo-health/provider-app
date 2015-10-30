7b9d2519784fc8ca8833d4d81b1abe53e81aeb28
Wuang Qin <wuang@leohealth.com>
2015-10-30 13:39:22 -0400

    set up bloodhound js

--- src/scripts/components/modules/search/findFamily.jsx
+++ src/scripts/components/modules/search/findFamily.jsx
@@ -1,11 +1,32 @@
 var React = require('react');
 
 module.exports = React.createClass({
+  getInitialState: function(){
+    return {query: ''}
+  },
+
+  componentDidMount: function(){
+    var suggests = new Bloodhound({
+      initialize: false,
+      local: ['dog', 'pig', 'moose'],
+      queryTokenizer: Bloodhound.tokenizers.whitespace,
+      datumTokenizer: Bloodhound.tokenizers.whitespace
+    });
+
+    var promise = suggests.initialize();
+
+    promise
+    .done(function() { console.log('ready to go!'); })
+    .fail(function() { console.log('err, something went wrong :('); });
+  },
+
   render: function () {
     return (
       <div className="panel panel-heading">
         <form className="form">
-          <input type="text" className="form-control" placeholder="Find family"/>
+          <input type="text"
+                 className="form-control"
+                 placeholder="Find family"/>
         </form>
       </div>
     )
