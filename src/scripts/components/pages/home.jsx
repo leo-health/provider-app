51e4abae83909e5551359a3c81b567dc28bb71ba
Wuang Qin <wuang@leohealth.com>
2016-02-05 17:50:58 -0500

    fix the problem Uncaught SyntaxError: Unexpected token u

--- src/scripts/components/pages/home.jsx
+++ src/scripts/components/pages/home.jsx
@@ -11,7 +11,7 @@ var _ = require('lodash');
 module.exports = React.createClass({
   componentWillMount: function(){
     this.pusher = new Pusher(leo.PUSHER_APPLICATION_KEY, {encrypted: true});
-    var email = JSON.parse(localStorage.user).email;
+    if (localStorage.user) var email = JSON.parse(localStorage.user).email;
     this.stateChannel = this.pusher.subscribe('newState' + email);
     this.messageChannel = this.pusher.subscribe('newMessage' + email);
   },
