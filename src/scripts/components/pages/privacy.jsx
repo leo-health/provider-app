531ed96a15dd78d9a8dc8d8cc3304fe2e3f6d665
Nayan Jain <nayan@leohealth.com>
2016-02-22 12:31:49 -0500

    241 - Adding the privacy policy and backlinks

---
+++ src/scripts/components/pages/privacy.jsx
@@ -0,0 +1,72 @@
+var React = require('react');
+
+module.exports = React.createClass({
+  render: function(){
+    return(
+        <div>
+            <h1>Leo Health - Privacy Policy</h1>
+            <p className="text-muted">Last updated: February 22, 2016</p>
+
+            <p>Leo Health respects the privacy of its website visitors and users.  This Privacy Policy governs the use of the Leo Health website located at www.leohealth.com (the "Website"), our mobile application ("App"), as well as any of our other websites, networks, embeddable widgets, downloadable software, or mobile or tablet computer applications, features, or services, whether on-line or off-line, owned or operated by us and on which a link to this Privacy Policy is displayed.  This Privacy Policy is provided to help you better understand how we obtain, use, share, and protect your personally identifiable information.  For purposes of this Privacy Policy your personally identifiable information also includes any personally identifiable information that you submit or consent to provide to Leo Health for your minor children under the age of thirteen.  The terms "you" and "your" shall also include any minor children for which you use the Leo Health Service.  Please take a moment to review our Privacy Policy, which covers the information Leo Health collects when you use the Leo Health Services (the "Leo Health Services").  By using this Website, you agree to the Leo Health Terms of Service and this Privacy Policy.  Leo Health may revise this Privacy Policy at any time in its sole discretion, and any revisions shall be effective as of the date of publication.  By continuing to use the Leo Health Services, you agree to any new revisions to this Privacy Policy.</p>
+            <p>Leo Health collects and stores information about our users to verify identity.  This information enables Leo Health to communicate with you directly and in turn provide you the Leo Health Services.
+</p>
+            <p>In this Privacy Policy, the terms "Leo Health," "we," "us," and "our" refers to Leo Health, Inc., and its affiliates.  The terms "you," "your," and "yours" refer to the user (end user) utilizing the Leo Health Services.  This Privacy Policy is incorporated into and subject to our Terms of Service located <a href="/terms">here</a>.</p>
+
+            <h3>Information We Collect</h3>
+            <p><strong>Personally Identifiable Information.</strong> If you voluntarily submit information to the Website, for example, in a request for general information, in order to create an account with us or populate your account, we may record and use such personally identifiable information, such as your name, phone number, e-mail address, and health care related claims information, for reasonable business purposes including, but not limited to, fulfilling your requests.  We may use internal service providers to operate the Website and employ other persons to perform work on our behalf, such as sending postal mail and e-mail.  These persons may have access to the personally identifiable information you submit through the Website, but only for the purpose of performing their duties.  These persons may not use your personally identifiable information for any other purpose.  Notwithstanding the preceding sentence, we may de-identify your personally identifiable information or aggregate your personally identifiable information with other users of the Leo Health Services.</p>
+            <p><strong>Non-Personally Identifiable Information.</strong> his Website also collects non-personally identifiable user information, such as IP addresses, cookies, internet tags and navigational data (server log files).  We anonymize this data and use it for system management and to improve the content and navigation of the Website.  We may collect this information passively using technologies such as standard server logs, cookies, and clear GIFs (also known as "Web beacons").  We use passively-collected information to administer, operate, and improve the Website and the Leo Health Services and to provide information that is customized for you.  If we link or associate any information gathered through passive means with personally identifiable information, or if applicable laws require us to treat any information gathered through passive means as personally identifiable information, we treat the combined information as personally identifiable information under this Privacy Policy.  Otherwise, we use and disclose information collected by passive means in aggregate form or otherwise in as non-personally identifiable information.</p>
+            <p><strong>Mobile Device Information.</strong> We may collect information about you from your mobile device through our App, including the identifier for any mobile devices, the operating system of your device (iOS vs. Android), user settings, and geo-location information.  By using our App on your mobile device some of your medical information may be stored on the device.  We may use the information provided by your mobile device, including your geo-location information, to make recommendations and personalize the App for you.  Leo Health is not responsible for any usage of your mobile device or our App by an unauthorized third party.</p>
+
+            <h3>About the Information We Collect</h3>
+            <p><em>Voluntarily-Provided Information</em><br />We collect information that you voluntarily provide when you sign up for Leo Health.  We also collect other information you provide to us through other means when using the Leo Health Services, such as when you fill out or update a user profile on the Leo Health Services or when you contact us to request help or support.</p>
+            <p><em>Information from your provider</em><br />We receive data from your provider’s electronic health record system.  We will present this data to you as part of our user interface. </p>
+
+            <h3>Our Use of Your Information</h3>
+            <p>We use your information to provide you with:</p>
+            <div className="list-group">
+                <a href="#" className="list-group-item">
+                    The Leo Health Services, including your Leo Health account.
+                </a>
+                <a href="#" className="list-group-item">
+                    Direct communications with you, which may include messages regarding Leo Health Services we believe you may be interested in.
+                </a>
+                <a href="#" className="list-group-item">
+                    Surveys about your provider, lifestyle habits, wellness, and other relevant information.
+                </a>
+                <a href="#" className="list-group-item">
+                    Customer service.
+                </a>
+            </div>
+            <p>We also use information we collect from you to prevent fraudulent use of the Leo Health Services and other systems; to prevent or take action against activities that are, or may be, in violation of our Terms of Service, or applicable law; for other administrative purposes; and for other purposes to which you may provide your consent.</p>
+
+            <h3>How We Share Your Information.</h3>
+            <p>Except as described in this Privacy Policy, we will not disclose personally identifiable information that we collect through the Leo Health Services to third parties without your consent.  We may disclose personally identifiable information to third parties if you consent to us doing so, for example to your healthcare providers and their staff and vendors (also known as business associates).</p>
+            <p>Any personally identifiable information you submit to us is for exclusive use by Leo Health or other entities that are involved in the operation or fulfillment purposes of this Website, including your healthcare providers and their staff and vendors.  It will not be shared with any other party, except (i) if required by law, subpoena or court order or to protect our legal rights, (ii) in connection with a change of control or sale of Leo Health’s assets or outstanding shares or similar transaction, as well as in the event of an insolvency, bankruptcy, or receivership, (iii) to service providers who perform services on our behalf and who are under confidentiality obligations, or (iv) as otherwise provided in this Privacy Policy.  We can (and you authorize us to) disclose any such information in those circumstances.  We do not sell, trade or license personally identifiable information about our users.</p>
+            <p>We may disclose aggregated or other types of non-personally identifiable information, including "automatically collected" and "cookies" information that are non-personally identifiable, to third parties for various purposes.  For example, we may disclose non-personally identifiable information to interested third parties to assist them in understanding the usage, viewing, and demographic patterns for certain programs, content, promotions, or functionality of the Leo Health Services. We may de-identify personally identifiable information and use it for marketing, advertising, research, software development, Leo Health reporting purposes, and to insurance companies and other payors.</p>
+
+            <h3>Security</h3>
+            <p>While Leo Health cannot guarantee the security of your personally identifiable information under all circumstances, Leo Health does take reasonable steps to protect your personally identifiable information and other information from loss, misuse, or unauthorized alteration by using industry-recognized security safeguards, coupled with carefully developed security procedures and practices. Whenever we download or transmit sensitive information, such as health information, we encrypt that information.  Any personal information and health information that we store is also encrypted to protect against unauthorized access.</p>
+            <p>However, please remember that the Internet is not a 100% secure environment, and therefore Leo Health cannot and does not guarantee that information you provide through the Leo Health Services may not be accessed, disclosed, altered, or destroyed by breach of any of our physical, technical, or managerial safeguards.</p>
+
+            <h3>Parents and Guardians</h3>
+            <p>The personally identifiable information on the Website is provided based on parental/guardian permission of minors using the services of participating healthcare providers and such parental/guardian consent to use the Leo Health Services.  Any information maintained by Leo Health about minors is provided only under the express authorization of the parent/guardian.  Leo Health does not solicit personally identifiable information from minors and will not contact minors directly.  Individuals over the age of thirteen are not permitted to use the Leo Health Services and all such accounts will be disabled.</p>
+
+            <h3>International Users</h3>
+            <p>The Leo Health Services are hosted in the United States and generally are intended for United States visitors.  If you visit from the European Union or other regions with laws governing data collection and use that may differ from United States law, please be aware that you are transferring personally identifiable information to the United States.  The United States does not have the same data protection laws as the European Union and other regions.  By providing personally identifiable information to us, you consent to the transfer of it to the United States and the use of it in accordance with this Privacy Policy and applicable law.</p>
+
+            <h3>Updates to This Policy</h3>
+            <p>We may occasionally update this Privacy Policy.  When we do, we will also revise the "last updated" date at the beginning of the Privacy Policy.  Your continued use of the Leo Health Services after such changes will be subject to the then-current Privacy Policy.  If we change this Privacy Policy in a manner that is materially less restrictive of our use or disclosure of your personally identifiable information, we will use reasonable efforts to notify you of the change and to obtain your consent prior to applying the change to any personally identifiable information that we collected from you prior to the date the change becomes effective.  We encourage you to periodically review this Privacy Policy to stay informed about how we collect, use, and disclose personally identifiable information.</p>
+
+            <h3>Any Questions?</h3>
+            <p>If you have any questions or comments about this Privacy Policy, please contact us using the following contact information:</p>
+            <blockquote>
+                <p>Leo Health, Inc.<br />
+                16 W 22nd Street<br />
+                6th Floor<br />
+                New York, NY 10010<br />
+                <a href="mailto:info@leohealth.com?Subject=Privacy%20Policy%20Inquiry" target="_top">info@leohealth.com</a></p>
+            </blockquote>
+        </div>
+    )
+  }
+});
