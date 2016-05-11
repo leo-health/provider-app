314bb4eefc8ccae2979d976af4c78dc8605a9967
Wuang Qin <wuang@leohealth.com>
2016-05-11 14:02:56 -0400

    add input filed auto advance on payment info

--- src/scripts/components/modules/registration/paymentInfoForm.jsx
+++ src/scripts/components/modules/registration/paymentInfoForm.jsx
@@ -1,4 +1,5 @@
 var React = require('react'),
+    ReactDom = require('react-dom'),
     RegistrationActions = require('../../../actions/registrationActions');
 
 module.exports = React.createClass({
@@ -9,6 +10,12 @@ module.exports = React.createClass({
 
   componentDidMount: function(){
     Stripe.setPublishableKey('pk_test_LRYSNRBvOYUG47Sg4QZqtlkB');
+  },
+
+  autoFormAdvance: function(size,currentFormId,nextFormId) {
+    if(document.getElementById(currentFormId).value.length===size) {
+      document.getElementById(nextFormId).focus();
+    }
   },
 
   render: function(){
@@ -28,7 +35,13 @@ module.exports = React.createClass({
               <div className="col-md-7 col-md-offset-1">
                 <div className="row">
                   <div className="form-group col-sm-8">
-                    <input type="text" className="form-control" size="20" data-stripe="number" placeholder="Card Number" ref="cardNumber"/>
+                    <input type="text"
+                           className="form-control"
+                           size="20" data-stripe="number"
+                           placeholder="Card Number"
+                           ref="cardNumber"
+                           required
+                           autoFocus/>
                   </div>
                   <div className="form-group col-sm-4">
                     <input type="text" className="form-control" size="5" data-stripe="address_zip" placeholder="Zip Code" ref="zip"/>
@@ -37,15 +50,35 @@ module.exports = React.createClass({
 
                 <div className="row">
                   <div className="form-group col-sm-4">
-                    <input type="text"  className="form-control" size="2" data-stripe="exp_month" placeholder="Expiration (MM)" ref="expirationMonth"/>
-                  </div>
-                  <span className="form-group col-sm-1"> / </span>
-                  <div className="form-group col-sm-4">
-                    <input type="text"  className="form-control" size="2" data-stripe="exp_year" placeholder="Expiration (YY)" ref="expirationYear"/>
+                    <input type="text"
+                           className="form-control"
+                           id="expirationMonth"
+                           size="2"
+                           data-stripe="exp_month"
+                           placeholder="Expiration (MM)"
+                           ref="expirationMonth"
+                           onKeyUp={()=>this.autoFormAdvance(2, "expirationMonth", "expirationYear")} />
                   </div>
 
-                  <div className="form-group col-sm-3">
-                    <input type="text"  className="form-control" size="4" data-stripe="cvc" placeholder="CVC" ref="cvc"/>
+                  <div className="form-group col-sm-4">
+                    <input type="text"
+                           className="form-control"
+                           id="expirationYear"
+                           size="2"
+                           data-stripe="exp_year"
+                           placeholder="Expiration (YY)"
+                           ref="expirationYear"
+                           onKeyUp={()=>this.autoFormAdvance(2, "expirationYear", "cvc")}/>
+                  </div>
+
+                  <div className="form-group col-sm-4">
+                    <input type="text"
+                           className="form-control"
+                           id="cvc"
+                           size="4"
+                           data-stripe="cvc"
+                           placeholder="CVC"
+                           ref="cvc"/>
                   </div>
                 </div>
               </div>
