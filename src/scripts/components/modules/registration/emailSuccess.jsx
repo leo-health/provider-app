71239bfd22df3149867e4973089ba9cd1dca8d47
Nayan Jain <nayan@leohealth.com>
2016-06-20 12:21:20 -0400

    no issue - missing file

---
+++ src/scripts/components/modules/registration/emailSuccess.jsx
@@ -0,0 +1,24 @@
+var React = require('react'),
+    Reflux = require('reflux'),
+    ReactRouter = require('react-router'),
+    {browserHistory} = ReactRouter;
+
+module.exports = React.createClass({
+  getInitialState: function(){
+    return { display: false }
+  },
+
+  render: function(){
+    return(
+      <div className="page-header row">
+        <div className="col-lg-offset-4 col-lg-4 col-lg-offset-4">
+            <div className="text-center"><img src="../../images/leo.png"/>
+              <h3>E-mail Confirmed</h3>
+              <p className="lead">Thank you for confirming your e-mail address. If you have any questions or need assistance feel free to reach out to us at support@leohealth.com.</p>
+              <a href="https://itunes.apple.com/app/id1051397244" target="_blank"><img className="lead" src="../images/download.png"/></a>
+            </div>
+        </div>
+      </div>
+    )
+  }
+});
