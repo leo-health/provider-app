34d8bc4b68244f7ff2c21d368239baabc5360000
Alex Chen <alexander.chen@yale.edu>
2016-07-06 13:18:02 -0400

    Add recipient field styling

--- src/scripts/components/modules/messageNote/recipientField.jsx
+++ src/scripts/components/modules/messageNote/recipientField.jsx
@@ -6,7 +6,13 @@ module.exports = React.createClass({
   render: function() {
     return (
       <div className="recipient-field-container">
-        <span className="glyphicon glyphicon-info-sign"
+        <div className="pull-left to-field">
+          <span className="to-field--to">To:</span>
+          <div className="to-field--parents">
+            <span className="glyphicon glyphicon-user"></span>
+          </div>
+        </div>
+        <span className="glyphicon glyphicon-info-sign pull-right note-toggler"
               onClick={this.props.onToggleInformation}></span>
       </div>
     )
