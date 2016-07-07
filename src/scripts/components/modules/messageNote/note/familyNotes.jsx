7160bfabadca4389b31ffa746eadbf74bf8ee348
Alex Chen <alexander.chen@yale.edu>
2016-07-07 18:30:26 -0400

    Add initial child info

--- src/scripts/components/modules/messageNote/note/familyNotes.jsx
+++ src/scripts/components/modules/messageNote/note/familyNotes.jsx
@@ -1,13 +1,44 @@
 var React = require('react');
 var Reflux = require('reflux');
 var _ = require('lodash');
+var moment = require('moment');
 
 module.exports = React.createClass({
 
   render: function() {
+    var guardians = this.props.guardians;
+    var patients = this.props.patients;
+    var phoneList = null;
+    var patientList = null;
+    if (guardians) {
+      phoneList = guardians.map(function(guardian, i){
+        return (
+          <div key={i} className="medium-font-size">{guardian.first_name} <span className="glyphicon glyphicon-earphone"></span> {guardian.email} </div>
+        )
+      });
+    }
+
+    if (patients) {
+      console.log(patients);
+      patientList = patients.map(function(patient, i){
+        var birthday = patient.birth_date;
+        var age = moment().diff(birthday, 'years');
+        console.log(birthday);
+        return (
+          <div>
+            <div className="heavy-font-size">{patient.first_name}, {patient.sex}</div>
+            <div className="medium-font-size">Age: {age}</div>
+          </div>
+
+        )
+      });
+    }
+
     return (
       <div className="family-notes-container col-lg-3 pre-scrollable panel-body">
         <h4><strong>Family Details</strong></h4>
+        {phoneList}
+        {patientList}
       </div>
     )
   }
