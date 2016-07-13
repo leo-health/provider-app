90fcaa1020dbb482c376cda92a92fa8dfa8c56ed
Alex Chen <alexander.chen@yale.edu>
2016-07-13 14:22:19 -0400

    Add vertical scaling

--- src/scripts/components/pages/footer.jsx
+++ src/scripts/components/pages/footer.jsx
@@ -6,7 +6,7 @@ module.exports = React.createClass({
       <div>
         <footer>
           <div className="row">
-            <div className="col-lg-12">
+            <div className="col-lg-12 footer-row">
               <ul className="list-unstyled">
                 <li className="pull-right medium-font-size feedback"><a href="mailto:support@leohealth.com">Feedback?</a></li>
                 <li><span className="medium-font-size">Need help? Call (206) 819-8549</span></li>
