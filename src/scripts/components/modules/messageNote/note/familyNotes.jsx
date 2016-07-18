7f6a668063484d8acf1b63da0b555e0f04fa1b9d
Alex Chen <alexander.chen@yale.edu>
2016-07-18 11:53:55 -0400

    Change family note names to full names

--- src/scripts/components/modules/messageNote/note/familyNotes.jsx
+++ src/scripts/components/modules/messageNote/note/familyNotes.jsx
@@ -17,7 +17,7 @@ module.exports = React.createClass({
       }
       return (
         <div key={i}>
-          <div className="heavy-font-size child-title"><strong>{patient.first_name}, {patient.sex}:</strong></div>
+          <div className="heavy-font-size child-title"><strong>{patient.first_name} {patient.last_name}, {patient.sex}:</strong></div>
           <div className="medium-font-size">Age: {age}</div>
         </div>
       )
@@ -32,7 +32,7 @@ module.exports = React.createClass({
       var matched = preProcessed.match(/^(\d{3})(\d{3})(\d{4})$/);
       if (matched) { phone = "(" + matched[1] + ") " + matched[2] + "-" + matched[3]; }
       return (
-        <div key={i} className="medium-font-size">{guardian.first_name} <span className="glyphicon glyphicon-earphone"></span> {phone} </div>
+        <div key={i} className="medium-font-size">{guardian.first_name} {guardian.last_name} <span className="glyphicon glyphicon-earphone"></span> {phone} </div>
       )
     });
     return phoneList;
